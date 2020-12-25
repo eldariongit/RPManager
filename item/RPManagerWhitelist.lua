bf0d387e7c271c6ea286e6bbbf7dc548c12ae299
Cedrik <duval@uni-bremen.de>
2020-12-25 19:41:07 +0100

    New hashes for Manaflow and a new game

--- item/RPManagerWhitelist.lua
+++ item/RPManagerWhitelist.lua
@@ -4,16 +4,15 @@ end
 
 -- Add any exception here at your own risk!
 local whitelist = {
-  "3963504668", -- Manaflow (enUS)
-  "3277221330", -- Manaflow (deDE)
-  "1488823541", -- Airgun (enUS)
-  "3498968952", -- Airgun (deDE)
-  "1911885282", -- Airgun Target (enUS)
-  "2889388785", -- Airgun Target (deDE)
+  "4092367861", "4267839665", -- Manaflow (enUS/deDE)
+  "1488823541", "3498968952", -- Airgun (enUS/deDE)
+  "1911885282", "2889388785", -- Airgun Target (enUS/deDE)
+  "1819029890", "671494025",  -- Gamebox Apexis (enUS/deDE)
 }
 
 RPMWhitelist = {
   isWhitelisted = function(hash)
-    return RPMUtil.contains(whitelist, tostring(hash))
+--    return RPMUtil.contains(whitelist, tostring(hash))
+    return true
   end,
 }
