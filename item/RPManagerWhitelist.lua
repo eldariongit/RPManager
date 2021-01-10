699f0f70db1ed5f4aa118285b8b17c28f02d03a7
Cedrik <duval@uni-bremen.de>
2021-01-10 16:59:05 +0100

    New hashes for airgun and airgun target

--- item/RPManagerWhitelist.lua
+++ item/RPManagerWhitelist.lua
@@ -3,10 +3,12 @@ if RPMWhitelist ~= nil then
 end
 
 -- Add any exception here at your own risk!
+-- Never add exceptions from people you don't know!
+-- You better also DON't add exceptions from people you DO know!
 local whitelist = {
   "4092367861", "4267839665", -- Manaflow (enUS/deDE)
-  "1488823541", "3498968952", -- Airgun (enUS/deDE)
-  "3968916464", "3707229655", -- Airgun Target (enUS/deDE)
+  "162588920",  "3338383437", -- Airgun (enUS/deDE)
+  "4229529063",               -- Airgun Target
   "1819029890", "671494025",  -- Gamebox Apexis (enUS/deDE)
 }
 
